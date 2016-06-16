86cf77bef2ee835e1927f22f5ee5ca6da08b0a18
vishnuprasadv <vishnuprasad.yes@gmail.com>
2016-06-16 19:22:36 +0530

    changes

--- src/main/java/com/vishnuprasadv/jtweet/App.java
+++ src/main/java/com/vishnuprasadv/jtweet/App.java
@@ -1,13 +1,25 @@
 package com.vishnuprasadv.jtweet;
 
+import com.vishnuprasadv.jtweet.data.DataBot;
+import com.vishnuprasadv.jtweet.data.SearchQuery;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+	public static void main(String[] args) throws Exception {
+		System.setProperty("http.proxyHost", "proxy.cognizant.com");
+		System.setProperty("http.proxyPort", "6050");
+		System.setProperty("https.proxyHost", "proxy.cognizant.com");
+		System.setProperty("https.proxyPort", "6050");
+		System.setProperty("http.nonProxyHosts", "localhost|127.0.0.1");
+		System.setProperty("https.nonProxyHosts", "localhost|127.0.0.1");
+		SearchQuery query = new SearchQuery("jonwestenberg", "2010-01-01", "2016-01-01");
+		String queryString = query.buildSearchQuery();
+		String json = new DataBot("https://twitter.com/i/search/timeline").retrieveData(queryString);
+		System.out.println(json);
     }
+
 }
--- src/main/java/com/vishnuprasadv/jtweet/data/SearchQuery.java
+++ src/main/java/com/vishnuprasadv/jtweet/data/SearchQuery.java
@@ -1,5 +1,8 @@
 package com.vishnuprasadv.jtweet.data;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 /**
  * @author Vishnu Prasad
  *
@@ -8,21 +11,104 @@ public final class SearchQuery {
 	
 	private String query;
 	
+	private String userName;
+	private String fromDate;
+	private String untilDate;
+	private String searchTerm;
+	
+	private static final String SUFFIX = "src=typd&max_position=";
+	
+
 	public SearchQuery(){
-		query = "";
+
 	}
 
-	public SearchQuery addParameter(String key, String value) {
-		this.query = this.query + "&" + key + ":" + value;
-		return this;
+	public String buildSearchQuery() throws UnsupportedEncodingException {
+		return "?f=realtime&q=" + URLEncoder.encode(this.addParameter("from", this.userName)
+				+ this.addParameter("since", this.fromDate) + this.addParameter("until", this.untilDate), "UTF-8")
+				+ SearchQuery.SUFFIX;
 	}
 
+	public String buildSearchQuery(String searchTerm) throws UnsupportedEncodingException {
+		return "?f=realtime&q="
+				+ URLEncoder.encode(this.addParameter("from", this.userName) + this.addParameter("since", this.fromDate)
+						+ this.addParameter("until", this.untilDate) + " " + searchTerm, "UTF-8")
+				+ "&" + SearchQuery.SUFFIX;
+	}
 
 	/**
-	 * @return the uri
+	 * @param userName
+	 * @param fromDate
+	 * @param untilDate
 	 */
-	public String getUri() {
-		return "?" + query;
+	public SearchQuery(String userName, String fromDate, String untilDate) {
+		this.userName = userName;
+		this.fromDate = fromDate;
+		this.untilDate = untilDate;
+	}
+
+	private String addParameter(String key, String value) {
+		return this.query + "&" + key + ":" + value;
+	}
+
+	/**
+	 * @return the userName
+	 */
+	public String getUserName() {
+		return userName;
+	}
+
+	/**
+	 * @param userName
+	 *            the userName to set
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	/**
+	 * @return the fromDate
+	 */
+	public String getFromDate() {
+		return fromDate;
+	}
+
+	/**
+	 * @param fromDate
+	 *            the fromDate to set
+	 */
+	public void setFromDate(String fromDate) {
+		this.fromDate = fromDate;
+	}
+
+	/**
+	 * @return the untilDate
+	 */
+	public String getUntilDate() {
+		return untilDate;
+	}
+
+	/**
+	 * @param untilDate
+	 *            the untilDate to set
+	 */
+	public void setUntilDate(String untilDate) {
+		this.untilDate = untilDate;
+	}
+
+	/**
+	 * @return the searchTerm
+	 */
+	public String getSearchTerm() {
+		return searchTerm;
+	}
+
+	/**
+	 * @param searchTerm
+	 *            the searchTerm to set
+	 */
+	public void setSearchTerm(String searchTerm) {
+		this.searchTerm = searchTerm;
 	}
 
 }
